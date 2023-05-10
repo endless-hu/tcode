505575228e2b70c424bda2905e5fce6014c7eeca
Zhijing Hu <66413488+endless-hu@users.noreply.github.com>
2023-05-10 13:40:32 +0800

    Complete first fit allocator

--- src/allocators/ff.cc
+++ src/allocators/ff.cc
@@ -2,5 +2,19 @@
 
 int FirstFitAllocator::allocate(std::vector<Item>& items
                                 __attribute__((unused))) {
-  return 0;
+  for (auto& item : items) {
+    bool allocated = false;
+    for (auto& bin : bins_) {
+      if (bin.fits(item)) {
+        bin.add_item(item);
+        allocated = true;
+        break;
+      }
+    }
+    if (!allocated) {
+      bins_.emplace_back(item.dim());
+      bins_.back().add_item(item);
+    }
+  }
+  return bins_.size();
 }
